dee43beb2cc494068d34840b50332626f3ce3dfb
Aldian Fernanda <zortagundul@gmail.com>
2024-02-14 15:23:48 +0700

    chore(configuration): :wrench: extend fonts configuration, adding "Inter" from Google Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -10,6 +12,9 @@ export default {
                 "anti-flash-white": "#F2F2F2",
                 "satin-sheen-gold": "#CA9D42",
                 eggshell: "#F4EBD9",
+            },
+            fontFamily: {
+                sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
             },
         },
     },
