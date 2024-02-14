240e1ba9784a921eef43129a28c6d033aab48e44
Aldian Fernanda <zortagundul@gmail.com>
2024-02-14 14:52:00 +0700

    chore(configuration): :wrench: update configuration, add custom colors
    Extends tailwind theme custom colors for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "darken-jungle-green": "#0B1F2A",
+                "metallic-sunburst": "#AA823C",
+                lotion: "#FBFBFB",
+                "anti-flash-white": "#F2F2F2",
+                "satin-sheen-gold": "#CA9D42",
+                eggshell: "#F4EBD9",
+            },
+        },
     },
     plugins: [],
 };
