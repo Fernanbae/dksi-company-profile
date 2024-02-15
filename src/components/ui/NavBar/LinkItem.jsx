132f35f63b57f3da58e6aa12a2b21079e54381f8
Aldian Fernanda <zortagundul@gmail.com>
2024-02-15 22:43:23 +0700

    feat(component): :sparkles: update navbar working on and responsive state
    Create links, button and related NavBar component. & Make NavBar responsive state (hamburger menu on mobile devices)

---
+++ src/components/ui/NavBar/LinkItem.jsx
@@ -0,0 +1,77 @@
+import PropTypes from "prop-types";
+
+function LinkItem({
+    title,
+    href,
+    highlight,
+    dropdown,
+    dropdownActive,
+    onToggle,
+    className,
+    children,
+}) {
+    const itemClasses =
+        "rounded-none md:rounded-md px-0 md:px-3 py-3.5 md:py-2 font-medium text-darken-jungle-green transition md:hover:bg-gray-200 w-full md:w-fit md:border-none border-b border-slate-200";
+    const itemHighlightClasses =
+        "text-center rounded-md px-3 py-2 text-white font-medium bg-gradient-to-r from-metallic-sunburst to-satin-sheen-gold w-full md:w-fit relative group";
+    return (
+        <li
+            className={`relative flex md:static ${dropdown ? "flex-col" : undefined} ${className}`}
+        >
+            {!dropdown ? (
+                <a
+                    href={href}
+                    className={highlight ? itemHighlightClasses : itemClasses}
+                >
+                    {title}
+                    {highlight && (
+                        <span className="absolute left-0 top-0 block h-full w-full rounded-md bg-gradient-to-b from-white/25 to-white/10 opacity-0 transition group-hover:opacity-100" />
+                    )}
+                </a>
+            ) : (
+                <>
+                    <button
+                        type="button"
+                        onClick={onToggle}
+                        className={`${itemClasses} inline-flex justify-between md:justify-normal`}
+                    >
+                        <span className="mr-1.5">{title}</span>
+                        <div
+                            className={`transition ${dropdownActive ? "rotate-180" : undefined}`}
+                        >
+                            <i
+                                className={`
+                                ri-arrow-down-s-line arrow-down-solutions text-darken-jungle-green/50 transition-all
+                                ${dropdownActive ? "font-bold text-metallic-sunburst" : undefined}
+                                `}
+                            ></i>
+                        </div>
+                    </button>
+                    {/* <div
+                        className={`absolute left-0 top-full ${dropdownActive ? "" : "hidden"} w-full overflow-hidden transition`}
+                    >
+                        {children}
+                    </div> */}
+                    {dropdownActive && (
+                        <div className="static left-0 top-full w-full md:absolute">
+                            {children}
+                        </div>
+                    )}
+                </>
+            )}
+        </li>
+    );
+}
+
+LinkItem.propTypes = {
+    title: PropTypes.string,
+    href: PropTypes.string,
+    highlight: PropTypes.bool,
+    dropdown: PropTypes.bool,
+    dropdownActive: PropTypes.bool,
+    onToggle: PropTypes.func,
+    className: PropTypes.string,
+    children: PropTypes.node,
+};
+
+export default LinkItem;
--- src/components/ui/NavBar/index.jsx
+++ src/components/ui/NavBar/index.jsx
@@ -2,9 +2,13 @@ import PropTypes from "prop-types";
 
 import TopBar from "./TopBar";
 
+import logoCrown from "../../../assets/sprites/logo-crown.svg";
+import logoText from "../../../assets/sprites/logo-text.svg";
+import LinkLists from "./LinkLists";
+
 function NavBar({ topBar }) {
     return (
-        <header className="sticky top-0 z-10">
+        <header className="sticky top-0 z-10 border-b border-red-200">
             {topBar && (
                 <TopBar href="https://mediaindonesia.com/humaniora/560191/pt-dksi-luncurkan-solusi-peralatan-telemedice-teranyar">
                     {
@@ -12,8 +16,20 @@ function NavBar({ topBar }) {
                     }
                 </TopBar>
             )}
-            <nav className="max-container flex h-20 justify-between">
-                {/* TODO: Create navigation links and company logo */}
+            <nav className="max-container flex h-20 items-center justify-between bg-white">
+                <a href="/" className="inline-flex items-center gap-4">
+                    <img
+                        src={`${logoCrown}`}
+                        alt="A regal crown design representing DKSI, with intricate details and a majestic presence."
+                        className="w-10"
+                    />
+                    <img
+                        src={`${logoText}`}
+                        alt="A black and white image of the letters 'DKSI' written in a bold font on a plain background."
+                        className="hidden w-16 sm:block"
+                    />
+                </a>
+                <LinkLists />
             </nav>
         </header>
     );
